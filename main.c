a1a55ae1108f4fc7087b93dcfd68723ec6e640be
Rymdskora <105083997+Rymdskora@users.noreply.github.com>
2024-02-04 22:54:01 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  int number = 8128;
+  int numberList[10000];
+  int arrayPosition = 0;
+  int total = 0;
+  int i;
+
+  
+  for (i = 1; i < number; i++) {
+    if (number % i == 0) {
+      numberList[arrayPosition] = i;
+      arrayPosition++;
+    }
+  }
+
+  for (i = 0; i < arrayPosition; i++) {
+    total += numberList[i];
+  }
+
+  if (total == number) {
+    printf("Input: %d\n\nPerfect Number.", number);
+  }
+  else {
+    printf("Input: %d\n\nNot A Perfect Number.", number);
+  }
+    
+  return 0;
+}
